a70e645ae30bc8c03b51e19c72816f1ae102ae1d
Brian Tsoi <brian.s.tsoi@gmail.com>
2023-02-18 18:19:06 -0500

    Reorgnize code

--- data_from_sensors/data_from_sensors.ino
+++ data_from_sensors/data_from_sensors.ino
@@ -1,6 +1,3 @@
-//int sensor1 = A0;
-//int sensor2 = A1;
-
 const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
 const int photoresistorPin = A0; // Photoresistor Pin 
@@ -14,10 +11,15 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // Starting Serial Terminal
   pinMode(photoresistorPin, INPUT);// Set pResistor - A0 pin as an input (optional)
-  //pinMode(sensor1, INPUT);
-  //pinMode(sensor2, INPUT);
   pinMode(pingPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  //print out column headers
+  Serial.print("Distance_sensor(cm)");
+  Serial.print(",");
+  Serial.print("Photoresistor(10-bit value)");
+  Serial.print(",");
+  Serial.println("Photoresistor(lux)");
 }
 
 //globals
@@ -25,25 +27,7 @@ int distance, brightness; //store data from both sensors
 double luxBrightness; // store lux value 
 int freq = 1000; //data collection frequency ~x milliseconds
 
-//global - store the header names in CSV file
-String dataLabel1 = "Distance_sensor(cm)";
-String dataLabel2 = "Photoresistor(10-bit value)";
-String dataLabel3 = "Photoresistor(lux)";
-bool label = true;
-
 void loop() {
-  //print out column headers
-  while(label){ //runs once
-    Serial.print(dataLabel1);
-    Serial.print(",");
-    Serial.print(dataLabel2);
-    Serial.print(",");
-    Serial.println(dataLabel3);
-    label=false; // print label once at the start 
-  }
-
-  // put your main code here, to run repeatedly:
-
   // distance sensor code
   long duration, inches, cm;
   digitalWrite(pingPin, LOW);
@@ -64,9 +48,6 @@ void loop() {
   distance = cm;
   brightness = analogValue;
   luxBrightness = luminosity;
-
-  //distance = analogRead(sensor1);
-  //brightness = analogRead(sensor2);
 
   //Display Data to Serial Monitor
   Serial.print(distance);
